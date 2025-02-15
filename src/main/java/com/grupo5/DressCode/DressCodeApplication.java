1cc0a8d326c3981cb139b8fef06ac53774bb2a89
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-02-14 22:34:08 -0300

    Primer commit de DresCode

---
+++ src/main/java/com/grupo5/DressCode/DressCodeApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5.DressCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressCodeApplication.class, args);
+	}
+
+}
