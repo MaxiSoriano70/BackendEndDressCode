f921c28bc533b70b6b2031a52ec4c3bd9ec69e04
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-02-27 03:50:00 -0300

    Agregando Erol

--- src/main/java/com/grupo5/DressCode/security/entity/User.java
+++ src/main/java/com/grupo5/DressCode/security/entity/User.java
@@ -1,10 +1,15 @@
 package com.grupo5.DressCode.security.entity;
 
+import com.grupo5.DressCode.utils.ERol;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @Builder
@@ -12,10 +17,42 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User implements UserDetails{
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer usuarioId;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private ERol rol;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
---
+++ src/main/java/com/grupo5/DressCode/utils/ERol.java
@@ -0,0 +1,6 @@
+package com.grupo5.DressCode.utils;
+
+public enum ERol {
+    ADMIN,
+    USER
+}
