1df47888816a47a163841433eb23d68f550aa353
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-02-27 03:43:34 -0300

    Back end con base de datos mysql

---
+++ src/main/java/com/grupo5/DressCode/security/entity/User.java
@@ -0,0 +1,21 @@
+package com.grupo5.DressCode.security.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer usuarioId;
+    private String email;
+    private String password;
+}
