a5dda10bb5b4380462a9336af91ae88889917d20
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-02-27 03:46:19 -0300

    Agregando dependencias y comentarios

--- src/main/java/com/grupo5/DressCode/security/entity/User.java
+++ src/main/java/com/grupo5/DressCode/security/entity/User.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer usuarioId;
